9f51f2455ab3318f3c21e645941beb707c1796fc
mutlu şimşek <mutlusims3k@gmail.com>
2024-06-07 18:32:56 +0300

    fix macos build and black formatting

--- scripts/run-python-tests.ps1
+++ scripts/run-python-tests.ps1
@@ -1,4 +1,6 @@
 Set-Location python-package
+black python/perpetual/
+black tests/
 maturin develop --release
 pytest .
 Set-Location ..
--- scripts/run-python-tests.sh
+++ scripts/run-python-tests.sh
@@ -1,4 +1,6 @@
 cd python-package
+black python/perpetual/
+black tests/
 maturin develop --release
 pytest .
 cd ..
