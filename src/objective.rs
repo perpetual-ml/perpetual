43497d5b079584514f080e0ee98c73972a5341ef
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-05-11 11:22:34 +0200

    :rocket: Adaptive Huber Loss
    This is a really dirty implementation of the Adaptive Huber Loss Function:
    
        1. Delta is determined by the 0.5th quantile of the residual distribution.
            - For quantiles between two data-points the lowest value is preferred.
    
    The implementation (largely) follows that of lightgbm.

--- src/objective.rs
+++ src/objective.rs
@@ -1,4 +1,4 @@
-use crate::{data::FloatData, metrics::Metric, utils::fast_sum};
+use crate::{data::FloatData, metric::Metric, utils::fast_sum};
 use serde::{Deserialize, Serialize};
 
 type ObjFn = fn(&[f64], &[f64], Option<&[f64]>, Option<f64>) -> (Vec<f32>, Option<Vec<f32>>);
@@ -9,6 +9,7 @@ pub enum Objective {
     LogLoss,
     SquaredLoss,
     QuantileLoss,
+    AdaptiveHuberLoss
 }
 
 pub fn loss_callables(objective: &Objective) -> LossFn {
@@ -16,6 +17,7 @@ pub fn loss_callables(objective: &Objective) -> LossFn {
         Objective::LogLoss => LogLoss::calc_loss,
         Objective::SquaredLoss => SquaredLoss::calc_loss,
         Objective::QuantileLoss => QuantileLoss::calc_loss,
+        Objective::AdaptiveHuberLoss => AdaptiveHuberLoss::calc_loss,
     }
 }
 
@@ -24,6 +26,7 @@ pub fn gradient_hessian_callables(objective: &Objective) -> ObjFn {
         Objective::LogLoss => LogLoss::calc_grad_hess,
         Objective::SquaredLoss => SquaredLoss::calc_grad_hess,
         Objective::QuantileLoss => QuantileLoss::calc_grad_hess,
+        Objective::AdaptiveHuberLoss => AdaptiveHuberLoss::calc_grad_hess
     }
 }
 
@@ -32,6 +35,7 @@ pub fn calc_init_callables(objective: &Objective) -> fn(&[f64], Option<&[f64]>, 
         Objective::LogLoss => LogLoss::calc_init,
         Objective::SquaredLoss => SquaredLoss::calc_init,
         Objective::QuantileLoss => QuantileLoss::calc_init,
+        Objective::AdaptiveHuberLoss => AdaptiveHuberLoss::calc_init,
     }
 }
 
@@ -322,6 +326,151 @@ impl ObjectiveFunction for QuantileLoss {
     }
 }
 
+/// Adaptive Huber Loss
+/// 
+/// 
+#[derive(Default)]
+pub struct AdaptiveHuberLoss {}
+impl ObjectiveFunction for AdaptiveHuberLoss {
+
+    // calculate loss
+    fn calc_loss(y: &[f64], yhat: &[f64], sample_weight: Option<&[f64]>, quantile: Option<f64>, ) -> Vec<f32> {
+
+        // default alpha: 0.5
+        // if not passed explicitly
+        let alpha = quantile.unwrap_or(0.5);
+        let n = y.len();
+
+        // absolute residuals
+        let mut abs_res: Vec<f64> = y.iter()
+            .zip(yhat)
+            .map(|(&yi, &yh)| (yi - yh).abs())
+            .collect();
+
+        // calculate delta
+        abs_res.sort_by(|a,b| a.partial_cmp(b).unwrap());
+        let idx = (n as f64 * alpha).floor() as usize;
+        let idx = idx.min(n - 1);
+        let delta = abs_res[idx];
+        
+        // sample-wise loss:
+        let mut out = Vec::with_capacity(n);
+        match sample_weight {
+            Some(w) => {
+                for i in 0..n {
+                    let r = y[i] - yhat[i];
+                    let ar = r.abs();
+                    let base = if ar <= delta {
+                        0.5 * r * r
+                    } else {
+                        delta * (ar - 0.5 * delta)
+                    };
+                    out.push((base * w[i]) as f32);
+                }
+            }
+            None => {
+                for i in 0..n {
+                    let r = y[i] - yhat[i];
+                    let ar = r.abs();
+                    let loss = if ar <= delta {
+                        0.5 * r * r
+                    } else {
+                        delta * (ar - 0.5 * delta)
+                    };
+                    out.push(loss as f32);
+                }
+            }
+        }
+
+        return out;
+    }
+
+    // calculate gradient and hessians
+    fn calc_grad_hess(y: &[f64], yhat: &[f64], sample_weight: Option<&[f64]>,quantile: Option<f64>,) -> (Vec<f32>, Option<Vec<f32>>) {
+
+        // default alpha: 0.5
+        // if not passed explicitly
+        let alpha = quantile.unwrap_or(0.5);
+        let n = y.len();
+
+        // absolute residuals
+        let mut abs_res: Vec<f64> = y.iter()
+            .zip(yhat)
+            .map(|(&yi, &yh)| (yi - yh).abs())
+            .collect();
+
+        abs_res.sort_by(|a,b| a.partial_cmp(b).unwrap());
+
+        // extract delta
+        let idx = (n as f64 * alpha).floor() as usize;
+        let idx = idx.min(n - 1);
+        let delta = abs_res[idx];
+
+        let mut grad = Vec::with_capacity(n);
+        let mut hess = Vec::with_capacity(n);
+        match sample_weight {
+            Some(w) => {
+                for i in 0..n {
+                    let r = y[i] - yhat[i];
+                    let ar = r.abs();
+                    let g = if ar <= delta {
+                        (yhat[i] - y[i]) * w[i]
+                    } else {
+                        delta * (yhat[i] - y[i]).signum() * w[i]
+                    };
+                    let h = if ar <= delta { w[i] } else { 0.0 };
+                    grad.push(g as f32);
+                    hess.push(h as f32);
+                }
+                (grad, Some(hess))
+            }
+            None => {
+                for i in 0..n {
+                    let r = y[i] - yhat[i];
+                    let ar = r.abs();
+                    let g = if ar <= delta {
+                        (yhat[i] - y[i])
+                    } else {
+                        delta * (yhat[i] - y[i]).signum()
+                    };
+                    let h = if ar <= delta { 1.0 } else { 0.0 };
+                    grad.push(g as f32);
+                    hess.push(h as f32);
+                }
+                (grad, Some(hess))
+            }
+        }
+    }
+
+    fn calc_init(y: &[f64], sample_weight: Option<&[f64]>, _quantile: Option<f64>,) -> f64 {
+
+        let mut idxs: Vec<usize> = (0..y.len()).collect();
+        idxs.sort_by(|&i, &j| y[i].partial_cmp(&y[j]).unwrap());
+        let total_w = sample_weight
+            .map(|w| w.iter().sum::<f64>())
+            .unwrap_or(y.len() as f64);
+        let target = total_w * 0.5;
+        
+        let mut cum = 0.0;
+        for &i in &idxs {
+            cum += sample_weight.map_or(1.0, |w| w[i]);
+            if cum >= target {
+                return y[i];
+            }
+        }
+
+        y[idxs[y.len()/2]]
+
+    }
+
+    fn default_metric() -> Metric {
+        Metric::RootMeanSquaredError
+    }
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -396,5 +545,25 @@ mod tests {
         let l2 = QuantileLoss::calc_init(&y, Some(&w), Some(0.9));
         println!("{}", l2);
         assert!(l2 == 9.0);
+    }
+
+    #[test]
+    fn test_adaptive_huberloss_loss() {
+        let y = vec![0.0, 0.0, 0.0, 1.0, 1.0, 1.0];
+        let yhat1 = vec![-1.0, -1.0, -1.0, 1.0, 1.0, 1.0];
+        let l1 = AdaptiveHuberLoss::calc_loss(&y, &yhat1, None, Some(0.5));
+        let yhat2 = vec![0.0, 0.0, -1.0, 1.0, 0.0, 1.0];
+        let l2 = AdaptiveHuberLoss::calc_loss(&y, &yhat2, None, Some(0.5));
+        assert!(l1.iter().sum::<f32>() > l2.iter().sum::<f32>());
+    }
+
+    #[test]
+    fn test_adaptive_huberloss_grad() {
+        let y = vec![0.0, 0.0, 0.0, 1.0, 1.0, 1.0];
+        let yhat1 = vec![-1.0, -1.0, -1.0, 1.0, 1.0, 1.0];
+        let (g1, _) = AdaptiveHuberLoss::calc_grad_hess(&y, &yhat1, None, Some(0.5));
+        let yhat2 = vec![0.0, 0.0, -1.0, 1.0, 0.0, 1.0];
+        let (g2, _) = AdaptiveHuberLoss::calc_grad_hess(&y, &yhat2, None, Some(0.5));
+        assert!(g1.iter().sum::<f32>() < g2.iter().sum::<f32>());
     }
 }
