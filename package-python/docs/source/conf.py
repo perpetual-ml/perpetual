4fbe273a76b31d266e4869ff8546865cfdf9bda3
Mutlu Simsek <mutlusims3k@gmail.com>
2026-02-02 00:08:31 +0300

    increment version to v1.1.1

--- package-python/docs/source/conf.py
+++ package-python/docs/source/conf.py
@@ -8,7 +8,7 @@ copyright = f"{datetime.now().year}, Mutlu Simsek, Serkan Korkmaz, Pieter Pel"
 author = "Mutlu Simsek, Serkan Korkmaz, Pieter Pel"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.0"
+release = "1.1.1"
 
 # -- General configuration ---------------------------------------------------
 
